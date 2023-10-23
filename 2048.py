d19e696952ee962ee43741b5e2f0e2c345619cc6
LAPTOP-KPBT0BRP <lovelypeem1158@gmail.com>
2023-10-23 14:38:11 +0700

    add colors.py

--- 2048.py
+++ 2048.py
@@ -1,7 +1,30 @@
-import tkinter as tk44
+import tkinter as tk
+import colors as c
 
 class Game(tk.Frame):
     def _init_(self):
         tk.Frame._init_(self)
         self.grid()
         self.master.title('2048')
+
+        self_main_grid = tk.Frame(self, bg=c.GRID_COLOR, bd=3,width=600, height=600)
+        self_main_grid(pady=(100,0))
+
+        def make_GUI(self): #make grid
+            self.cell = []
+            for i in range(4):
+                row = []
+                for j in range(4):
+                    cell_frame = tk.Frame(
+                        self.main_grid,
+                        bg=c.EMPTY_CELL_COLOR,
+                        width=150,
+                        height=150
+)
+                    
+                    cell_frame.grid(row=i,column=j, padx=5,cnf=builtins.dict, **kw)
+                    cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                    cell_number.grid(row=i, column=j)
+                    cell_data = {'frame': cell_frame, 'number': cell_number}
+                    row.append(cell_data)
+                    self.cells.appenf(row)
