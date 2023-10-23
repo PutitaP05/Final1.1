9571c352e9dd4c75d351b96dd01ea223ab5901f5
LAPTOP-KPBT0BRP <lovelypeem1158@gmail.com>
2023-10-23 16:49:13 +0700

    def start_game

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 print('I just got vscode.')
-print('With my new labtob becuse the old one want to retire themself so bad.')
+print('With my new labtob because the old one want to retire themself so bad.')
